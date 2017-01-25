df75c9e91b01d00971567a159beb2b6e2a8aa715
ashu <jainamritanshu@gmail.com>
2017-01-25 17:26:43 +0530

    csrf exempt

--- main/views.py
+++ main/views.py
@@ -46,8 +46,8 @@ def Register(request):
 		password = request.POST['password']
 		confirm_password = request.POST['confirm_password']
 
-		registered_users = UserProfile.objects.all()
-		registered_contacts = [x.contact for x in registered_users]
+		registered_users = User.objects.all()
+		registered_contacts = [x.username for x in registered_users]
 		if password == confirm_password:
 			if contact in registered_contacts:
 				if type(contact) is int:	

387cac6470ef0ea5a8b306a53c3900c2891acb31
ashu <jainamritanshu@gmail.com>
2017-01-26 20:50:51 +0530

    node chat

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^accounts/login/social/$', views.social_login, name='social_login'),
     url(r'^accounts/login/$', views.login_user, name='login'),
     url(r'^user/logout/$', views.logout_user, name='logout'),
+    url(r'^chat/test/$', views.test_chat, name='test_chat'),
     url(r'^accounts/register/$', views.Register, name='Register'),
     url(r'^user/nick/$', views.nick_name, name='nick_name'),
     url(r'^user/interests/$', views.interests, name='interests'),
@@ -14,5 +15,6 @@ urlpatterns = [
     url(r'^room/(?P<room_name>[\w-]{,50})/get_members/$', views.get_members_chatroom, name='get_members_chatroom'),
     url(r'^user/anonymous/$', views.go_anonymous, name='anonymous'),
     url(r'^room/(?P<room_name>[\w-]{,50})/$', views.get_chatroom, name='get_chatroom'),
-    url(r'^room/(?P<group_name>[\w-]{,50})/message_send/$', views.node_api_message, name='node_api_message'),
+    # url(r'^room/(?P<group_name>[\w-]{,50})/message_send/$', views.node_api_message, name='node_api_message'),
+    url(r'^node_api/$', views.test_node_api, name='node_api_message'),
 ]

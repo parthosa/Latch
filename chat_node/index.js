c254101005327dbe4b920cbab45c04f61d81ae0a
ashu <jainamritanshu@gmail.com>
2017-07-23 12:30:53 +0530

    base_url

--- chat_node/index.js
+++ chat_node/index.js
@@ -11,37 +11,14 @@
 // app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/index.html');
 // });
-
-// // io.on('connection', function(socket){
-// //   console.log('a user connected');
-// //   socket.on('chat message', function(msg){
-// //     io.emit('chat message', msg);
-// //     console.log('message: ' + msg);
-// //   });
-// // });
-
-// // http.listen(3000, function(){
-// //   console.log('listening on *:3000');
-// // });
-// io.sockets.on('connection', function (socket) {
-    
-//     //Grab message from Redis and send to client
-//     sub.on('message', function(channel, message){
-//         socket.send(message);
-//     });
-    
-//     //Client is sending message through socket.io
-//     socket.on('send_message', function (message) {
-//         values = querystring.stringify({
-//             comment: message,
-//             sessionid: socket.handshake.cookie['sessionid'],
 //         });
 
 
 
 var http = require('http');
 var request = require('request');
-var server = http.createServer().listen(4000, '172.16.1.139');
+base_url = '172.16.1.139';
+var server = http.createServer().listen(4000, base_url);
 var io = require('socket.io').listen(server);
 var cookie_reader = require('cookie');
 var querystring = require('querystring');
@@ -52,106 +29,19 @@ var sub = redis.createClient();
 //Subscribe to the Redis chat channel
 sub.subscribe('chat');
  
-//Configure socket.io to store cookie set by Django
-// io.configure(function(){
-//     io.set('authorization', function(data, accept){
-//         if(data.headers.cookie){
-//             data.cookie = cookie_reader.parse(data.headers.cookie);
-//             return accept(null, true);
-//         }
-//         return accept('error', false);
-//     });
-//     io.set('log level', 1);
-// });
 
-
-    // io.on('connection', function (socket) {
-        
-    //     //Grab message from Redis and send to client
-    //     sub.on('message', function(channel, message){
-    //         socket.send(message);
-    //     });
-        
-    //     //Client is sending message through socket.io
-    //     socket.on('send_message_group', function (data) {
-    //         // message_split=data.split(',');
-    //         // console.log(3);
-    //         // console.log(sessionid);
-    //         // console.log(socket.handshake.cookie['sessionid']);
-    //         // values = querystring.stringify({
-    //         //     data
-    //         //     // csrftoken:message[2]// sessionid: message[1],
-    //         //     // sessionid: socket.handshake.cookie['sessionid'],
-    //         // });
-    //         // console.log(values)
-    //         // request({
-                
-    //         //     method:'POST',
-    //         //     headers: {
-    //         //         'Content-Type': 'application/json',
-    //         //         'Cookie': 'csrftoken=' + message[2]
-    //         //     },
-    //         // },function(res){
-    //         //     console.log('worked');
-    //         // });
-    //         // request.post('http://localhost:8001/main/node_api/',{name:1});
-    //         var options = {
-    //             // host: 'localhost',
-    //             // port: 8001,
-    //             // path: '/main/node_api/',
-    //             url: 'http://localhost:8001/main/user/chat/indi/',
-    //             method: 'POST',
-    //             form: data,
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 // 'Content-Length': values.length,
-    //                 // "X-CSRFToken": message[2],
-    //                 // 'Cookie': 'csrftoken=' + message_split[2]
-    //             }
-    //         }
-    //         // console.log(6);
-    //         // var req = {}
-    //         // //Send message to Django server
-    //         request(options, function (error, response, body) {
-    //             if (!error && response.statusCode == 200) {
-    //                 // Print out the response body
-    //                 console.log(body)
-    //                 socket.send('response_group', data)
-    //             }
-    //             console.log(body);
-    //         });
-
-    //     });
 
 io.sockets.on('connection', function (socket) {
     console.log(35);
     socket.on('send_message_indi', function (data) {
         // message_split=data.split(',');
-        // console.log(sessionid);
-        // console.log(socket.handshake.cookie['sessionid']);
-        // values = querystring.stringify({
-        //     message
-        //     // csrftoken:message[2]// sessionid: message[1],
-        //     // sessionid: socket.handshake.cookie['sessionid'],
-        // });
-        // console.log(values)
-        // request({
-            
-        //     method:'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Cookie': 'csrftoken=' + message[2]
-        //     },
-        // },function(res){
-        //     console.log('worked');
-        // });
-        // request.post('http://localhost:8001/main/node_api/',{name:1});
+ 
         console.log(data);
         var options = {
             // host: 'localhost',
             // port: 8001,
             // path: '/main/node_api/',
-            url: 'http://172.16.1.139:8001/main/user/chat/indi/',
+            url: 'http://'+base_url+':8001/main/user/chat/indi/',
             method: 'POST',
             form: data,
             headers: {
@@ -176,55 +66,19 @@ io.sockets.on('connection', function (socket) {
         });
 });
 
-        // var req=http.request(options, function(res){
-        //     // res.setEncoding('utf8');
-        //     console.log(4);
-        //     //Print out error message
-        //     // res.on('data', function(message){
-        //     //     if(message != 'Everything worked :)'){
-        //     //         console.log('Message: ' + message);
-        //     //     }
-        //     // });
-        // });
-        // req.write('dsghgjh');
-        // req.end();
-        // request.post('http://localhost:8001/main/node_api/', {'comment': message[0]});
-        // while(req=={}){
-        //     console.log(7);
-        //         req.write(values);
-        //     }
-        // req.end();
+
     });
 
 io.sockets.on('connection', function (socket) {
     console.log(35);
     socket.on('send_message_group', function (data) {
         console.log(data);
-        // message_split=data.split(',');
-        // console.log(sessionid);
-        // console.log(socket.handshake.cookie['sessionid']);
-        // values = querystring.stringify({
-        //     message
-        //     // csrftoken:message[2]// sessionid: message[1],
-        //     // sessionid: socket.handshake.cookie['sessionid'],
-        // });
-        // console.log(values)
-        // request({
-            
-        //     method:'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Cookie': 'csrftoken=' + message[2]
-        //     },
-        // },function(res){
-        //     console.log('worked');
-        // });
-        // request.post('http://localhost:8001/main/node_api/',{name:1});
+
         var options = {
             // host: 'localhost',
             // port: 8001,
             // path: '/main/node_api/',
-            url: 'http://172.16.1.139:8001/main/room/message_send/',
+            url: 'http://'+base_url+':8001/main/room/message_send/',
             method: 'POST',
             form: data,
             headers: {
@@ -249,24 +103,7 @@ io.sockets.on('connection', function (socket) {
         });
 });
 
-        // var req=http.request(options, function(res){
-        //     // res.setEncoding('utf8');
-        //     console.log(4);
-        //     //Print out error message
-        //     // res.on('data', function(message){
-        //     //     if(message != 'Everything worked :)'){
-        //     //         console.log('Message: ' + message);
-        //     //     }
-        //     // });
-        // });
-        // req.write('dsghgjh');
-        // req.end();
-        // request.post('http://localhost:8001/main/node_api/', {'comment': message[0]});
-        // while(req=={}){
-        //     console.log(7);
-        //         req.write(values);
-        //     }
-        // req.end();
+
     });
 
 

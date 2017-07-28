d5f1505864d043625509c9a2e8a4497306767cbe
ashu <jainamritanshu@gmail.com>
2017-07-28 19:07:12 +0530

    deleting unnecessary folders

--- Latch_Ionic/script.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-ionic plugin add cordova-plugin-googlemaps --variable API_KEY_FOR_ANDROID="AIzaSyCMWU0rhBZA84UD_oecdjT3SehyHTs4zvs" --variable API_KEY_FOR_IOS="AIzaSyCMWU0rhBZA84UD_oecdjT3SehyHTs4zvs"
--- app_front/js/script.js
+++
@@ -1,7 +0,0 @@
-$('#sign-up').click(function (ev) {
-	 Materialize.toast('Try Again', 1000)
-})
-
-$('#sign-in').click(function (ev) {
-	 Materialize.toast('Try Again', 1000)
-})
